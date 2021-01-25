b2a4d9bb5e26b7fd9fda67d914978f86803723d2
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-01-25 15:09:38 +0100

    made a new println statement

--- src/no/dnb/reskill/MyNewJavaVlassUsingGit.java
+++ src/no/dnb/reskill/MyNewJavaVlassUsingGit.java
@@ -3,6 +3,7 @@ package no.dnb.reskill;
 public class MyNewJavaVlassUsingGit {
     public static void main(String[] args) {
         System.out.println("it runs");
+        System.out.println("making some additions to the program");
     }
 
 }
