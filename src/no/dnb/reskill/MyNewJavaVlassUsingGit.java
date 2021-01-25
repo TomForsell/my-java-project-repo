e292eb8295a3e25772bf57ffafb4f897becf610b
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-01-25 15:15:12 +0100

    added a new print statement

--- src/no/dnb/reskill/MyNewJavaVlassUsingGit.java
+++ src/no/dnb/reskill/MyNewJavaVlassUsingGit.java
@@ -4,6 +4,7 @@ public class MyNewJavaVlassUsingGit {
     public static void main(String[] args) {
         System.out.println("it runs");
         System.out.println("making some additions to the program");
+        System.out.println("I have branched and making some more additions");
     }
 
 }
